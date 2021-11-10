804de3ae2ac49d19cea39e48e1c22836d0bb6400
fabian.gutierrez <fabian.gutierrez@estudiante.uam.es>
2021-11-10 23:11:52 +0100

    Funciona, a priori (si es que los resultados son correctos)

--- P2/getTopSales.sql
+++ P2/getTopSales.sql
@@ -1,26 +1,45 @@
-/* Vista auxiliar */
-CREATE OR REPLACE VIEW top_sales_per_year AS
-    SELECT year, MAX(sales) as top_sales
-    FROM imdb_movies
-        NATURAL JOIN products
-        NATURAL JOIN inventory
-    GROUP BY imdb_movies.year;
-
 CREATE OR REPLACE FUNCTION getTopSales(year1 INT, year2 INT, OUT Year INT, OUT
     Film CHAR, OUT sales bigint) RETURNS SETOF RECORD AS $$
+DECLARE
+    min_year INT;
+    max_year INT;
 BEGIN
-    RETURN QUERY SELECT DISTINCT ON(imdb_movies.year, sales)
-            imdb_movies.year,
-            movietitle::CHAR(255),
-            inventory.sales::bigint
-    FROM imdb_movies
-        NATURAL JOIN products
-        NATURAL JOIN inventory
-    WHERE imdb_movies.year >= year1
-            AND imdb_movies.year <= year2
-            AND inventory.sales = (SELECT top_sales
-                         FROM top_sales_per_year AS tspy
-                         WHERE tspy.year = imdb_movies.year)
-    ORDER BY sales DESC;
+    min_year := LEAST(year1, year2);
+    max_year := GREATEST(year1, year2);
+    DROP TABLE IF EXISTS getTopSalesResults;
+    CREATE TABLE getTopSalesResults (
+        movieid INT,
+        Year INT,
+        sales bigint
+    );
+
+    WHILE min_year <= max_year LOOP
+        DROP TABLE IF EXISTS getTopSalesAux;
+        CREATE TABLE getTopSalesAux AS
+        SELECT movieid, SUM(quantity) as total_sales
+        FROM orders
+        INNER JOIN orderdetail ON (orders.orderid = orderdetail.orderid)
+        INNER JOIN products ON (orderdetail.prod_id = products.prod_id)
+        WHERE EXTRACT(year FROM orderdate) = min_year
+        GROUP BY movieid;
+
+        INSERT INTO getTopSalesResults
+        SELECT movieid, min_year AS Year, total_sales AS sales
+        FROM getTopSalesAux
+        WHERE total_sales = (SELECT MAX(total_sales) FROM getTopSalesAux);
+
+        min_year := min_year + 1;
+    END LOOP;
+
+    RETURN QUERY SELECT DISTINCT ON(getTopSalesResults.Year, getTopSalesResults.sales)
+    getTopSalesResults.Year,
+    movietitle::CHAR(255),
+    getTopSalesResults.sales
+    FROM getTopSalesResults
+    INNER JOIN imdb_movies ON (getTopSalesResults.movieid = imdb_movies.movieid)
+    ORDER BY getTopSalesResults.sales DESC;
+
+    DROP TABLE getTopSalesAux;
+    DROP TABLE getTopSalesResults;
 END;
 $$ LANGUAGE 'plpgsql';
