2f3284206046c3258ca0be1efbb0d3207c82ef1a
Fgp910 <f.gutierrezpena910@gmail.com>
2021-11-11 15:55:25 +0100

    Avanza en la implementación (A PASO DE VENCEDORES)

--- P2/updInventoryAndCustomer.sql
+++ P2/updInventoryAndCustomer.sql
@@ -3,11 +3,30 @@ AS $$
 DECLARE
     cart_prod RECORD;
     inventory_prod RECORD;
+    cust RECORD;
+    points_per_euro INT;
 BEGIN
+    points_per_euro = 100;
+
     IF OLD.status IS NOT NULL THEN    /* Is not a recently purchased cart */
         RETURN NULL;
     END IF;
 
+    /* Check balance or loyalty points are enough */
+    cust = (SElECT *
+                FROM customers
+                WHERE customers.customerid = NEW.customerid);
+    IF NEW.points THEN
+        IF cust.loyalty < NEW.totalamount * points_per_euro THEN
+            RAISE EXCEPTION 'Not enough loyalty points';
+        END IF;
+    ELSE
+        IF cust.balance < NEW.totalamount THEN
+            RAISE EXCEPTION 'Not enough funds in account';
+        END IF;
+    END IF;
+
+    /* Check inventory stock */
     DROP TABLE IF EXISTS purchased_products;
     CREATE TABLE purchased_products AS
     SELECT prod_id,
@@ -22,25 +41,34 @@ BEGIN
         inventory_prod = (SELECT *
                             FROM inventory
                             WHERE inventory.prod_id = cart_prod.prod_id);
-        /*IF EXISTS (SELECT NULL
-                    FROM inventory
-                    WHERE inventory.prod_id = cart_prod.prod_id
-                            AND stock < cart_prod.quantity) THEN*/
         IF inventory_prod.stock < cart_prod.quantity THEN
             RAISE EXCEPTION 'Not enough stock of %', cart_prod.prod_name;
         ELSIF inventory_prod.stock = cart_prod.quantity THEN
-            /* TODO: crear alarma */
+            INSERT INTO alerts(prod_id, alert_date, alert_time)
+            VAlUES (cart_prod.prod_id, CURRENT_DATE, LOCALTIME);
         END_IF;
     END LOOP;
 
-    /* UPDATE inventory */
+    /* Update tables */
+    UPDATE inventory
+    SET stock = stock - quantity,
+        sales = sales + quantity
+    FROM purchased_products
+    WHERE purchased_products.prod_id = inventory.prod_id;
+
+    IF NEW.points THEN
+        /* Update loyalty */
+    ELSE
+        /* Update balance */
+    END IF;
+
 
     DROP TABLE purchased_products;
     RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
 
-DROP TRIGGER IF EXISTS tr_updInventoryAndCustomer;
+DROP TRIGGER IF EXISTS tr_updInventoryAndCustomer ON orders;
 CREATE TRIGGER tr_updInventoryAndCustomer
 BEFORE UPDATE OF status ON orders
     FOR EACH ROW EXECUTE PROCEDURE updInventoryAndCustomer();
