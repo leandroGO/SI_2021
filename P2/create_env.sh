ea73aef8719fc2047dc939af008b329b4d31c5d0
fabian.gutierrez <fabian.gutierrez@estudiante.uam.es>
2021-11-14 14:49:26 +0100

    Fuerza a usar SQLAlchemy 1.4

--- P2/create_env.sh
+++ P2/create_env.sh
@@ -1,4 +1,4 @@
 virtualenv -p python3 si1pyenv
 source si1pyenv/bin/activate
-pip3 install Flask SQLAlchemy Flask-SQLAlchemy SQLAlchemy-Utils \
+pip3 install Flask SQLAlchemy==1.4 Flask-SQLAlchemy SQLAlchemy-Utils \
 psycopg2 itsdangerous Flask-Session
