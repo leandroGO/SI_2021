e12d7cb81447f140c5075095c8d316b6020a9b51
Leandro Garcia <leandro.garcia@estudiante.uam.es>
2021-11-24 03:47:18 -0800

    optimizacion query

--- P3/ciudadesDistintas.sql
+++ P3/ciudadesDistintas.sql
@@ -1,3 +1,12 @@
+create or replace function dateFormat(dateField date)
+returns
+	char(6)
+AS $$
+	SELECT to_char(dateField, 'YYYYMM')
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE INDEX indice on orders (dateFormat(orderdate))
+
 SELECT COUNT(*)
 FROM (SELECT DISTINCT city
 	FROM customers NATURAL JOIN
