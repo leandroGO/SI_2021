cd766b6333000ec34ed3273dc9320126dddb2b55
fabian.gutierrez <fabian.gutierrez@estudiante.uam.es>
2021-11-30 23:43:57 +0100

    Arregla bugs, sigue pendiente del sleep

--- P3/SQL/updPromo.sql
+++ P3/SQL/updPromo.sql
@@ -5,12 +5,14 @@ CREATE OR REPLACE FUNCTION updPromo() RETURNS TRIGGER
 AS $$
 BEGIN
     UPDATE orderdetail
-    SET price = ROUND(products.price * (1 - customer.promo/100.0), 2)
+    SET price = ROUND(products.price * (1 - NEW.promo/100.0), 2)
     FROM orders, products
     WHERE orders.customerid = NEW.customerid
         AND orders.orderid = orderdetail.orderid
         AND orders.status IS NULL
         AND products.prod_id = orderdetail.prod_id;
+
+    RETURN NULL;
 END;
 $$ LANGUAGE 'plpgsql';
 
